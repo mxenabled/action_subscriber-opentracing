eb4dae3386f9352145dc6cccdd01b8e3dfeb37f3
Marcos Minond <minond.marcos@gmail.com>
2019-06-11 08:28:40 -0600

    using standard operation name prefix and tags

--- lib/action_subscriber/opentracing/middleware.rb
+++ lib/action_subscriber/opentracing/middleware.rb
@@ -10,17 +10,20 @@ module ActionSubscriber
       end
 
       def call(env)
-        operation = "#{env.subscriber}##{env.action}"
+        operation = "Subscriber #{env.subscriber}##{env.action}"
         parent = ::OpenTracing.extract(::OpenTracing::FORMAT_TEXT_MAP, env.headers)
         published_at = env.headers["published_at"]
 
         options = {}
         options[:references] = [::OpenTracing::Reference.follows_from(parent)] if parent
-        options[:tags] = {}
-        options[:tags]["span.kind"] = "consumer"
-        options[:tags]["message_bus.destination"] = env.routing_key
+        options[:tags] = {
+          "span.kind" => "consumer",
+          "component" => "ActionSubscriber",
+          "message_bus.destination" => env.routing_key,
+          "message_bus.processed_at" => Time.now.strftime("%F %T.%3N %Z")
+        }
+
         options[:tags]["message_bus.published_at"] = published_at if published_at
-        options[:tags]["message_bus.processed_at"] = Time.now.strftime("%F %T.%3N %Z")
 
         result = nil
         ::OpenTracing.start_active_span(operation, options) do
